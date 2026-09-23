4bf0b8701d2e5e40502f8af1c051707d419abe65
Siddhant Sinha <330279753+SiddhantSinha-dev@users.noreply.github.com>
2026-09-23 20:37:13 +0530

    Add program for tuple unpacking in lists_tuples_and_sets

---
+++ lists_tuples_and_sets/tuple_creation_unpacking.py
@@ -0,0 +1,5 @@
+# Tuple creation and unpacking into different variables
+
+bio_data = ('Ryu Kanui', 23, 'Osaka')
+name, age, city = bio_data
+print('Welcome {}, age {} from the lovely city of {}.'.format(name, age, city))
